58229ac808250fec5d83c00b40275926c38c68ac
Hadrien Lejard <hadrien.lejard@gmail.com>
2017-05-29 20:35:00 +0200

    Update faye.dart

--- example/faye.dart
+++ example/faye.dart
@@ -1,10 +1,8 @@
 import 'package:gitter/src/faye.dart';
 import 'package:gitter/src/models/faye_message.dart';
 
-const token =
-    "c18d06c5bcadf6335c9d5d594b61c7883b433803"; // ?? const String.fromEnvironment("TOKEN");
-const roomId =
-    "57aa693540f3a6eec05eb1d5"; //const String.fromEnvironment("ROOM_ID");
+const token = const String.fromEnvironment("TOKEN");
+const roomId = const String.fromEnvironment("ROOM_ID");
 
 main() async {
   GitterFayeSubscriber subscriber = new GitterFayeSubscriber(token);
@@ -16,11 +14,11 @@ main() async {
     });
   };
 
-  //subscriber.subscribeToRoom(roomId);
-  //subscriber.subscribeToChatMessages(roomId);
-  //subscriber.subscribeToRoomEvents(roomId);
-  //subscriber.subscribeToRoomUsers(roomId);
-  //subscriber.subscribeToUser(subscriber.user.id);
+  subscriber.subscribeToRoom(roomId);
+  subscriber.subscribeToChatMessages(roomId);
+  subscriber.subscribeToRoomEvents(roomId);
+  subscriber.subscribeToRoomUsers(roomId);
+  subscriber.subscribeToUser(subscriber.user.id);
   subscriber.subscribeToUserRoomUnreadItems(roomId, subscriber.user.id);
 
   subscriber.listen(messageHandler);
